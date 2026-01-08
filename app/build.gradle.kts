203a72d75f11cb0b2bbdc688ba1a98ccce8bd3d3
Raveuk <raveuk@live.co.uk>
2026-01-08 16:30:29 +0000

    Bump version to 1.0.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "com.zimbabeats"
         minSdk = 24
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.0.8"
+        versionCode = 9
+        versionName = "1.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
