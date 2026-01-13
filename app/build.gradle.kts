7d71e5a32836ab9d5e87019f599625feb8c0855b
Raveuk <raveuk@live.co.uk>
2026-01-13 09:53:30 +0000

    Bump version to v1.0.20 - Material 3 Theme & Typography Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "com.zimbabeats"
         minSdk = 24
         targetSdk = 36
-        versionCode = 17
-        versionName = "1.0.17"
+        versionCode = 20
+        versionName = "1.0.20"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
